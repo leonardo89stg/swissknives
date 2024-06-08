b27bc88e4735cb69a39a8b6f9340e9ea69e24537
leonardo89stg <leo89stg@gmail.com>
2024-06-08 18:22:36 -0300

    bug fix

---
+++ app teste/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '75520ffa54308201b7e8db8afbf31ea1ea9195a0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '75520ffa54308201b7e8db8afbf31ea1ea9195a0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'leonardo89stg/swissknives' => array(
+            'pretty_version' => 'v1.0',
+            'version' => '1.0.0.0',
+            'reference' => '75520ffa54308201b7e8db8afbf31ea1ea9195a0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../leonardo89stg/swissknives',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
--- app teste/view.php
+++ app teste/view.php
@@ -1,2 +1,8 @@
 <?php
 require 'vendor/autoload.php';
+
+
+use Leonardo89stg\Swissknives\start;
+
+$dahs = new start();
+echo $dahs->ShowMsg();
